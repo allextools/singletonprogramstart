cce36684e793a7f045f74c811756e2cd8e9fbeac
Andrija Petrovic <andrija.hers@gmail.com>
2017-09-12 06:11:07 -0400

    Full asynchronous support with the introduction of cb and errorcb

--- index.js
+++ index.js
@@ -4,40 +4,40 @@ var npid = require('npid'),
     path = require('path'),
     pidfn;
 
-function nullPidFile(){
+function nullPidFile(cb,errorcb){
   try {
     fs.unlinkSync(pidfn);
   } catch (ignore) {
   }
-  createPidFile();
+  createPidFile(cb,errorcb);
 }
 
 function abort () {
   process.exit(1);
 }
 
-function checkRunningPid(cb){
+function checkRunningPid(cb,errorcb){
   try{
     var pid = parseInt(fs.readFileSync(pidfn).toString()), cbr;
     if(isNaN(pid)){
       nullPidFile();
     }else{
       if (isrunning(pid)) {
-        if (cb) {
-          cbr = cb(pid, pidfn);
+        if (errorcb) {
+          cbr = errorcb(pid, pidfn);
           if (cbr && ('function' === typeof cbr.then)) {
             cbr.then(
-              createPidFile,
+              createPidFile.bind(null, cb, errorcb),
               abort
             );
             return;
           }
-          createPidFile();
+          createPidFile(cb,errorcb);
         } else {
           abort();
         }
       } else {
-        nullPidFile();
+        nullPidFile(cb,errorcb);
       }
     }
   }
@@ -46,19 +46,20 @@ function checkRunningPid(cb){
   }
 }
 
-function createPidFile(cb){
+function createPidFile(cb,errorcb){
   try {
     var pidf = npid.create(pidfn);
     pidf.removeOnExit();
+    cb();
   }
-  catch(e){
-    checkRunningPid(cb);
+  catch(ignore){
+    checkRunningPid(cb,errorcb);
   }
 }
 
-function startPidControlledProgram(programname,cwd,cb){
+function startPidControlledProgram(programname,cwd,cb,errorcb){
   pidfn = path.join((cwd||__dirname),(programname||'pidcontrol')+'.pid');
-  createPidFile(cb);
+  createPidFile(cb,errorcb);
 };
 
 module.exports = startPidControlledProgram;
